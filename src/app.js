994d5f117fa97c4463e6a223d6acd5ac4d0aa7ba
Zekirija <zekirija2001@hotmail.com>
2021-04-24 03:15:06 +0200

    a

--- src/app.js
+++ src/app.js
@@ -1,7 +1,38 @@
 
 
-let documentation = null;
 
-fetch('https://api.github.com/repos/Zekiloni/focus-roleplay-hub/contents/documentation', { method: 'get', headers: new Headers({ 'Authorization': 'token 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', 'Content-Type': 'none' }) }) 
-    .then(response => response.json())
-    .then(data => console.log(data));
+
+let app = new Vue({
+    el: '#hud',
+    data: {
+        repo: 'https://api.github.com/repos/Zekiloni/focus-roleplay-hub/',
+        token: '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',
+        files: []
+
+    },
+
+    created() {
+        this.docs()
+    },
+
+    methods: { 
+        
+        docs: function() { 
+            fetch(this.repo + 'contents/documentation', { method: 'get', headers: new Headers({ 'Authorization': 'token ' + this.token, 'Content-Type': 'none' }) }) 
+                .then(response => response.json())
+                .then(data => this.stack(data))
+        },
+
+        stack: function (docs) { 
+            for (let i of docs) { 
+                if (i.type === 'file') { 
+                    this.files.push({ name: i.name, path: i.path })
+                } else if (i.type) { 
+                    
+                }
+            }
+            console.log(docs)
+        }
+
+    }
+})
