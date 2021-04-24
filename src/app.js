99423f5b0d03058691eed3e6584eb83f6a52747c
Zekirija <zekirija2001@hotmail.com>
2021-04-24 03:33:05 +0200

    new

--- src/app.js
+++ src/app.js
@@ -3,36 +3,45 @@
 
 
 let app = new Vue({
-    el: '#hud',
+    el: '#app',
     data: {
+
+        page: 'documentation',
+
         repo: 'https://api.github.com/repos/Zekiloni/focus-roleplay-hub/',
         token: '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',
+
+        directoriums: [],
         files: []
 
     },
 
     created() {
-        this.docs()
+        this.load(this.page)
     },
 
     methods: { 
         
-        docs: function() { 
-            fetch(this.repo + 'contents/documentation', { method: 'get', headers: new Headers({ 'Authorization': 'token ' + this.token, 'Content-Type': 'none' }) }) 
+        load: function(page) { 
+            this.page = page;
+            fetch(this.repo + 'contents/' + page, { method: 'get', headers: new Headers({ 'Authorization': 'token ' + this.token, 'Content-Type': 'none' }) }) 
                 .then(response => response.json())
                 .then(data => this.stack(data))
         },
 
         stack: function (docs) { 
+            console.log(docs)
+            this.files = [], this.directoriums = [];
             for (let i of docs) { 
                 if (i.type === 'file') { 
-                    this.files.push({ name: i.name, path: i.path })
-                } else if (i.type) { 
-                    
+                    this.files.push({ name: i.name, path: i.path });
+                } else if (i.type === 'dir') { 
+                    this.directoriums.push({ name: i.name, path: i.path });
                 }
             }
-            console.log(docs)
+
         }
+
 
     }
 })
